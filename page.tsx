b169d4640ecfd1a89478192b5e6b54273e2a4903
Shahjahan <137832488+shahjahanbrohigithub@users.noreply.github.com>
2024-10-29 01:22:56 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,30 +1,60 @@
-import Header  from "./components/Header/Header"
-import Footer  from "./components/Header/Footer";
-import React from 'react';
+// app/contact/page.ts
 
-const Home = () => {
+import React from 'react';
+import Header from '../components/Header/Header'
+import Footer  from "../components/Header/Footer";
+import StyleCss from './contact.module.css'
+const Contact = () => {
   return (
     <div>
-        <Header />
       
-        <div className="hero">
-        <div className="content">
-          <h1 className="title">Welcome to Auction Table</h1>
-          <p className="subtitle">
-            Buy, sell, and discover amazing deals in one place.
-          </p>
-          <a href="#" className="ctaButton">
-            Get Started
-          </a>
-        </div>
+      <Header></Header>
+
+      <div className={StyleCss.contact}>
+        <h2 style={{textAlign:"center",fontWeight:"30px"}}>Contact From</h2>
+        <br />
+        <form action="">
+          <label htmlFor="name">Name</label>
+          <input style={{border: "1px solid  gray ",
+             borderRadius:"7px",
+             width:"200px",
+             height:"25px"}} type="text" />
+             <br />
+             <br />
+               <label htmlFor="name">L/Name</label>
+          <input style={{border: "1px solid  gray ",
+             borderRadius:"7px",
+             width:"200px",
+             height:"25px"}} type="text" />
+             <br />
+             <br />
+               <label htmlFor="name">Email</label>
+          <input style={{border: "1px solid  gray ",
+             borderRadius:"7px",
+             width:"200px",
+             height:"25px"}} type="text" />
+             <br />
+             <br />
+               <label htmlFor="name">Phone</label>
+          <input style={{border: "1px solid  gray ",
+             borderRadius:"7px",
+             width:"200px",
+             height:"25px"}} type="text" />
+             <br />
+             <br />
+               <label htmlFor="name">Password</label>
+          <input style={{border: "1px solid  gray ",
+             borderRadius:"7px",
+             width:"200px",
+             height:"25px"}} type="text" />
+        </form>
+           <button style={{border: "1px solid gray", backgroundColor:"black", padding:"8px", borderRadius:"10px", color:"white",marginTop:"5px", marginLeft:"90px"}} type='submit'>Submit</button>
       </div>
-
-                <Footer></Footer>
+      
+      <Footer></Footer>
+      
     </div>
-
-
-                
   );
 };
 
-export default Home;
+export default Contact;
